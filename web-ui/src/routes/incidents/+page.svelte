3b711556f5b40e02e992ee2ef377e06db77208ba
Cam Stuart <cam.asoftware@gmail.com>
2023-12-24 19:52:18 +1100

    basic incident page

--- web-ui/src/routes/incidents/+page.svelte
+++ web-ui/src/routes/incidents/+page.svelte
@@ -2,15 +2,43 @@
 	import { page } from '$app/stores';
 
 	console.log('Incident page loaded: ', $page.url.pathname);
+
+
+	import { onMount } from 'svelte';
+
+	let incidents = [];
+	let error = null;
+
+	async function fetchIncidents() {
+		try {
+			const response = await fetch('http://localhost:3000/api/incidents');
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			incidents = await response.json();
+		} catch (err) {
+			error = err.message;
+		}
+	}
+
+	onMount(fetchIncidents);
+
+
 </script>
 
 <div class="container p-10 space-y-4">
 	{#if $page.data.session}
 		<h1>Incidents</h1>
 		<hr />
-		<p>
-			Fireops is a ....
-		</p>
+		{#if error}
+			<p>Error: {error}</p>
+		{:else}
+			<ul>
+				{#each incidents as incident}
+					<li>{incident.name} - {incident.description}</li>
+				{/each}
+			</ul>
+		{/if}
 	{:else}
 		<h1>Access Denied</h1>
 		<p>
